68ea6533d7ae54415aa7943c7d2f3951086d3530
kavin-333 <skb20062013@gmail.com>
2025-12-25 17:46:21 +0530

    Add GitHub Pages deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/BLOG-APP/',
+  base: '/blog-app/',
   plugins: [
     
     react(),
